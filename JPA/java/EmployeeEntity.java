91b5d88e3d88814b6b0a4cd85b6a5b5879e49684
JayantUkey <110605408+JayantUkey@users.noreply.github.com>
2022-08-16 11:13:44 +0530

    Create EmployeeEntity.java

---
+++ JPA/java/EmployeeEntity.java
@@ -0,0 +1,73 @@
+package com.employee;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employeeAssessment")
+public class EmployeeEntity {
+	
+	 @Id
+	  private int empno;
+	  private String name;
+	  private String designation;
+	  private double salary;
+	  private int age;
+	  
+	public EmployeeEntity(int empno, String name, String designation, double salary, int age) {
+		super();
+		this.empno = empno;
+		this.name = name;
+		this.designation = designation;
+		this.salary = salary;
+		this.age = age;
+	}
+
+	public EmployeeEntity() {
+		super();
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSal() {
+		return salary;
+	}
+
+	public void setSal(double salary) {
+		this.salary = salary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	  
+	 
+	
+}
